272a9b8f0609355ecc929dec95b94969a52ed62c
siham-wedeh <siham.wedeh@gmail.com>
2025-09-30 11:57:05 +0300

    about page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Your Partner for Business Success in the UAE and Beyond</title>
+        <title> @yield('title')</title>
         <link rel="stylesheet" href="/css/style.css?v={{ time() }}">
         <link rel="stylesheet" href="/css/respo.css?v={{ time() }}">
         <link rel="stylesheet" href="/css/swiper-bundle.min.css?v={{ time() }}">
